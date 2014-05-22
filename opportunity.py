468a7eac3a0cd737721679396cdec7543ea09e7c
NaN?tic <suport@nan-tic.com>
2014-05-22 15:46:50 +0200

    Fix bug when the party or its relationshsips have not phones

--- opportunity.py
+++ opportunity.py
@@ -40,7 +40,7 @@ class SaleOpportunityAsterisk(Wizard):
         mechanisms = list(set(mechanisms))
         return {
             'allowed_contacts_mechanisms': [m.id for m in mechanisms],
-            'contact_mechanisms': mechanisms[0].id
+            'contact_mechanisms': mechanisms and mechanisms[0].id or None
             }
 
     def transition_dial(self, values=False):
